027b5b873170de6ec388b6d454d913e1dfbdb045
Endless <0x3f@0x3f3f3f3f.com>
2026-05-12 19:24:15 +0800

    lc2126

--- leetcode/weekly/274/c/c.go
+++ leetcode/weekly/274/c/c.go
@@ -1,15 +1,45 @@
 package main
 
-import "sort"
+import (
+	"math"
+	"math/bits"
+	"slices"
+)
 
 // github.com/EndlessCheng/codeforces-go
-func asteroidsDestroyed(mass int, asteroids []int) bool {
-	sort.Ints(asteroids)
-	for _, v := range asteroids {
-		if mass < v {
+func asteroidsDestroyed1(mass int, asteroids []int) bool {
+	slices.Sort(asteroids)
+	for _, x := range asteroids {
+		if mass < x { // 无法摧毁小行星 x
 			return false
 		}
-		mass += v
+		mass += x // 获得这颗小行星的质量
+	}
+	return true
+}
+
+func asteroidsDestroyed(mass int, asteroids []int) bool {
+	maxWidth := bits.Len(uint(slices.Max(asteroids)))
+	sum := make([]int, maxWidth)
+	mn := make([]int, maxWidth)
+	for i := range mn {
+		mn[i] = math.MaxInt
+	}
+
+	for _, x := range asteroids {
+		i := bits.Len(uint(x)) - 1
+		sum[i] += x
+		mn[i] = min(mn[i], x)
+	}
+
+	for i, m := range mn {
+		if m == math.MaxInt {
+			continue
+		}
+		if mass < m { // 无法摧毁这组的任意小行星
+			return false
+		}
+		mass += sum[i] // 获得这组小行星的质量
 	}
 	return true
 }
