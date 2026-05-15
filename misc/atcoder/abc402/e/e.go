5533eac46e3568fd34b99c8cd837f890fea9c457
Endless <0x3f@0x3f3f3f3f.com>
2026-05-15 10:49:07 +0800

    abc402_e

---
+++ misc/atcoder/abc402/e/e.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	. "fmt"
+	"io"
+	"math/bits"
+)
+
+// https://github.com/EndlessCheng
+func run(in io.Reader, out io.Writer) {
+	var n, tot int
+	Fscan(in, &n, &tot)
+	a := make([]struct{ s, p float64; c int }, n)
+	for i := range a {
+		Fscan(in, &a[i].s, &a[i].c, &a[i].p)
+		a[i].p /= 100
+	}
+
+	f := make([][1 << 8]float64, tot+1)
+	for i := range f {
+		for mask := range 1 << n {
+			for s := uint8(mask); s > 0; s &= s - 1 {
+				j := bits.TrailingZeros8(s)
+				if c := a[j].c; c <= i {
+					f[i][mask] = max(f[i][mask], a[j].p*(f[i-c][mask^1<<j]+a[j].s)+(1-a[j].p)*f[i-c][mask])
+				}
+			}
+		}
+	}
+	Fprintf(out, "%.6f", f[tot][1<<n-1])
+}
+
+//func main() { run(os.Stdin, os.Stdout) }
